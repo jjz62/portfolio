7cea7342b55f7186feb18695a43f41fb58c7394d
Jenny Zhang <jjz62@cornell.edu>
2023-05-16 00:41:51 +0000

    styled top of homepage

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+<div class="container d-flex justify-content-between">
+    <a class="navbar-brand mb-0 h1 display-4" href="/">@zhangjiaying28 </a>
+    <ul class="navbar-nav">
+          <!-- <li class="nav-item active">
+            <a class="nav-link" href="/">@zhangjiaying28</a>
+          </li> -->
+    </ul>
+</div>
+</nav>
--- pages/home.php
+++ pages/home.php
@@ -13,13 +13,36 @@
 
 <body>
 
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-      <div class="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 class="display-4 font-weight-normal">Punny headline</h1>
-        <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
-        <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+<?php
+  include('includes/header.php')
+?>
+
+<div class="container position-relative overflow-hidden pt-5">
+  <div class="row gx-5 mx-auto">
+  <div class="col-3">
+    <img src="../public/uploads/home/profile.jpeg" class="rounded-circle" style="width: 100%;" alt="Girl with black hair wearing scarf">
+  </div>
+  <div class="col-8 align-self-center">
+        <div class="row d-flex align-items-end gx-0">
+          <div class="col">
+            <h1>Jenny Zhang</h1>
+          </div>
+          <!-- <div class="col-4">
+            <p class="text-muted">(she/her)</p>
+          </div> -->
+        </div>
+        <h5><small class="text-muted">Product Designer</small></h5>
+        <p class="lead">I create impact through my values of empathy, simplicity, and consistency. Passionate about UX in public-facing products for social good + game design!</p>
+        <p class="lead">
+          Learning ✍️ + Designing 🎨 <a href="https://cis.cornell.edu/" class="text-decoration-none">@cornelluniversity</a>. Incoming UX Design Intern <a href="https://www.capitalone.com/" class="text-decoration-none">@capitalone</a>.
+        </p>
       </div>
     </div>
+  </div>
+
+<section>
+
+</section>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </body>
