c0bae9ce78eac827200d3a369c91e0efc9a94ac6
Jenny Zhang <jjz62@cornell.edu>
2023-05-22 20:14:12 +0000

    complete topic roooms case study

--- pages/clubview.php
+++ pages/clubview.php
@@ -11,6 +11,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="../public/site.css">
+  <link rel="stylesheet" href="../public/footer.css">
 </head>
 
 <body>
@@ -21,9 +22,9 @@
 
 <div class="container">
     <div class="row">
-        <div class="contents pt-2">
+        <div class="contents">
             <nav id="navbar-cv" class="navbar navbar-light flex-column align-items-stretch p-3">
-                <h5 class="text-dark">Contents</h5>
+                <!-- <h5 class="text-dark">Contents</h5> -->
                     <nav class="nav nav-pills flex-column mt-0 pt-0">
                         <a class="nav-link" href="#item-1">Introduction</a>
                         <a class="nav-link" href="#item-2">Problem Space</a>
@@ -58,7 +59,7 @@
 
             <div class="container pl-0 ml-0 py-3">
                 <div class="row pt-3">
-                    <div class="col-4">
+                    <div class="col-2">
                         <h5>Details</h5>
                     </div>
                     <div class="col-2">
@@ -296,6 +297,9 @@
 
 </div>
 
+<?php
+  include('includes/footer2.php')
+?>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
