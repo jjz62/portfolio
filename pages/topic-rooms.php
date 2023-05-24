d5c457b663d2d1b5140d74bed0da4a9878dd75f6
Jenny Zhang <jjz62@cornell.edu>
2023-05-24 18:21:21 +0000

    added logo to browser tab

--- pages/topic-rooms.php
+++ pages/topic-rooms.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="../public/site.css">
   <link rel="stylesheet" href="../public/footer.css">
+  <link rel="icon" href="../public/uploads/favicon.png">
 </head>
 
 <body>
